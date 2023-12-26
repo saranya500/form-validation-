73288e83a1d0fe0aabe0b84b09181cd6b069f1f4
saranya500 <gsaranya554@gmail.com>
2023-12-26 21:58:33 +0530

    files created

---
+++ login.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Get input values
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    // Validate the username and password (dummy validation for example)
+    if (username === 'user' && password === 'password') {
+      // If the login is successful, redirect to the to-do list page
+      window.location.href = 'todo.html';
+    } else {
+      // Show an error message (in a real app, you'd handle this differently)
+      alert('Invalid username or password');
+    }
+  });
+  
